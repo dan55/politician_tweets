27168c419ffbf1e6ebc7088fd652593fd954e7d7
dan55 <bobo789@aol.com>
2017-07-08 09:51:19 -0400

    add some basic queries

---
+++ tweet.sql
@@ -0,0 +1,31 @@
+CREATE  EXTERNAL TABLE tweets
+(
+    value STRING 
+)
+LOCATION  '/user/w205/political_tweets/tweets';
+
+# following the approach recommended here: https://stackoverflow.com/a/13368942/6781104
+
+# Playing around: 
+#
+# SELECT created_at, screen_name 
+# FROM tweets t LATERAL VIEW 
+# JSON_TUPLE(t.value, 'created_at', 'screen_name') blah AS created_at, screen_name 
+# LIMIT 1;
+
+# Verifying number of users:
+#
+# SELECT COUNT(DISTINCT screen_name) 
+# FROM tweets t LATERAL VIEW 
+# JSON_TUPLE(t.value, 'screen_name') j AS screen_name;
+#
+# result: 545
+
+#
+#
+# SELECT screen_name, COUNT(screen_name) as cnt
+# FROM tweets t LATERAL VIEW
+# JSON_TUPLE(t.value, 'screen_name') j AS screen_name
+# GROUP BY screen_name
+# ORDER BY cnt DESC
+# LIMIT 20;
